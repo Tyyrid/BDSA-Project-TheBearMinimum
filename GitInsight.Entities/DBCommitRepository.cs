9331056eb0c638a3aa62cae8b53673301cfc983a
Turið Højgaard <tuho@itu.dk>
2022-11-06 15:08:00 +0100

    Tilføjet Core til sln

--- GitInsight.Entities/DBCommitRepository.cs
+++ GitInsight.Entities/DBCommitRepository.cs
@@ -1,5 +1,19 @@
 namespace GitInsight.Entities;
 
-/*public class DBCommitRepository : IDBCommitRepository {
+public class DBCommitRepository : IDBCommitRepository
+{
+    public (Response Response, int commitId) Create(DBCommitCreateDTO commit)
+    {
+        throw new NotImplementedException();
+    }
 
-}*/
+    public DBCommitDTO Read(int commitId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyCollection<DBCommitDTO> Read()
+    {
+        throw new NotImplementedException();
+    }
+}
