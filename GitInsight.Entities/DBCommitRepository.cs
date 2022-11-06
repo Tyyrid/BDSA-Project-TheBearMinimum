000ed77997201029fae4e598cd23e2a6f611abd5
Turið Højgaard <tuho@itu.dk>
2022-11-06 15:13:57 +0100

    Startet på repositories

--- GitInsight.Entities/DBCommitRepository.cs
+++ GitInsight.Entities/DBCommitRepository.cs
@@ -2,6 +2,13 @@ namespace GitInsight.Entities;
 
 public class DBCommitRepository : IDBCommitRepository
 {
+    readonly GitInsightContext context;
+
+    public DBCommitRepository(GitInsightContext context) {
+        this.context = context;
+    }
+
+
     public (Response Response, int commitId) Create(DBCommitCreateDTO commit)
     {
         throw new NotImplementedException();
---
+++ GitInsight.Entities/DBFrequencyRepository.cs
@@ -0,0 +1,19 @@
+namespace GitInsight.Entities;
+
+public class DBFrequencyRepository : IDBFrequencyRepository
+{
+    public (Response Response, int frequencyId) Create(DBFrequencyCreateDTO frequency)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DBFrequencyDTO Read(int frequencyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyCollection<DBFrequencyDTO> Read()
+    {
+        throw new NotImplementedException();
+    }
+}
